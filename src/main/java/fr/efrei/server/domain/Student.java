60525ce68070cd77f45d17cf664daa135192ce66
oussama Mahdjoubi <bilal.mahdjoubi95@gmail.com>
2023-12-09 11:00:36 +0100

    Add Student

---
+++ src/main/java/fr/efrei/server/domain/Student.java
@@ -0,0 +1,33 @@
+package fr.efrei.server.domain;
+
+public class Student {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}

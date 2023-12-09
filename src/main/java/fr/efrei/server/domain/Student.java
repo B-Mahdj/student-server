f931c02f2f894d66e932468487d451e969a3457f
oussama Mahdjoubi <bilal.mahdjoubi95@gmail.com>
2023-12-09 14:31:15 +0100

    Add fully functionnal webservices with local db integration

--- src/main/java/fr/efrei/server/domain/Student.java
+++ src/main/java/fr/efrei/server/domain/Student.java
@@ -1,11 +1,20 @@
 package fr.efrei.server.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
     private Integer id;
 
     private String name;
 
     private Integer age;
+
+    public Student() {
+    }
 
     public Integer getId() {
         return id;
@@ -27,7 +36,14 @@ public class Student {
         this.name = name;
     }
 
-    public void setAge(Integer age) {
-        this.age = age;
+    public void setAge(Integer age) {this.age = age;}
+
+    @Override
+    public String toString() {
+        return "Student{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", age=" + age +
+            '}';
     }
 }
